5e91ece7511bdae9a9a5c861a3b167c0800d527e
Tobias <tobias_maier99@gmx.de>
2021-12-07 23:27:08 +0100

    day 7

--- src/day07.rs
+++ src/day07.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 pub fn run() -> Result<(), Box<dyn Error>> {
     let filename = "../input07";
     let contents = fs::read_to_string(filename)?;
-    let v: Vec<&str> = contents.split("\n").collect();
+    let v: Vec<&str> = contents.split(",").collect();
     let ints: Vec<i32> = v.iter().filter(|line| {
         match **line {
             "" => false,
@@ -18,14 +18,39 @@ pub fn run() -> Result<(), Box<dyn Error>> {
         }
     }).collect();
 
+    let _tests = [16,1,2,0,4,2,7,1,2,14].to_vec();
+    println!("solve07(tests): {}", solve07(&_tests));
+    assert!(solve07(&_tests) == 37);
     println!("solve07: {:?}", solve07(&ints));
+    println!("solve07_2(tests): {}", solve07_2(&_tests));
+    assert!(solve07_2(&_tests) == 168);
     println!("solve07_2: {:?}", solve07_2(&ints));
 
     Ok(())
 }
 
-fn solve07(_v: &Vec<i32>) {
+fn solve07(positions: &Vec<i32>) -> i32 {
+    let mut result = 0;
+    let mut sorted = positions.clone();
+    sorted.sort();
+    let target = sorted[sorted.len() / 2];
+    for position in positions {
+        result += (position - target).abs();
+    }
+    result
 }
 
-fn solve07_2(_v: &Vec<i32>) {
+fn solve07_2(positions: &Vec<i32>) -> i32 {
+    let mut result = 0;
+    for target in 0..*positions.iter().max().unwrap() {
+        let mut res = 0;
+        for position in positions {
+            let n = (position - target).abs();
+            res += ((n * (n + 1)) / 2) as i32;
+        }
+        if result == 0 || res < result {
+            result = res;
+        }
+    }
+    result
 }
